fc9860e29334cdce810db4733189e5abab440ef5
abhiaiyer91 <abhiaiyer91@gmail.com>
2015-10-25 14:56:55 -0700

    es6

--- client/reactlion.jsx
+++ client/reactlion.jsx
@@ -1,17 +1,32 @@
-function randomRange(min, max) {
+/**
+ * Create a random range between numbers
+ * @param min
+ * @param max
+ * @returns {*}
+ */
+var randomRange = (min, max) => {
   return min + (Math.random() * max - min);
-}
+};
 
-function Point(x, y) {
-  this.x = x;
-  this.y = y;
-}
+/**
+ * Establish a point class
+ * @param x
+ * @param y
+ * @constructor
+ */
+var Point = class Point {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+  }
+};
+
 
 var lastMousePos = new Point(window.innerWidth * .5, window.innerHeight * .5); // assume the mouse starts at the center
 var mouseVelocity = new Point(Math.random() * (window.innerWidth * .4), Math.random() * (window.innerHeight * .4)); // base the initial mouse velocity off screensize
 var breakability = .9; // decimal between 0-1, closer to 1 the further polygons push away from eachother
 
-setInterval(function () { // wait 4 seconds then start listening to mouse move
+Meteor.setInterval(() => { // wait 4 seconds then start listening to mouse move
   document.body.onmousemove = function (e) {
     var mousePos = new Point(e.screenX, e.screenY); // current mouse position
     // calculate moues velocity based on distance between frames, breakability, and window size
@@ -21,11 +36,12 @@ setInterval(function () { // wait 4 seconds then start listening to mouse move
   }
 }, 4000);
 
-Polygon = React.createClass({
-  getInitialState: function () {
-    return {
-      origFill: this.props.fill, // store the original fill color
-      origPoints: this.props.points, // store the original positions that the polygon should be attracted to
+Polygon = class Polygon extends React.Component {
+  constructor(props) {
+    super();
+    this.state = {
+      origFill: props.fill, // store the original fill color
+      origPoints: props.points, // store the original positions that the polygon should be attracted to
       darkAmnt: 0, // a number we will continue to increment and that then take the Math.sin() value of
       colorVelocity: randomRange(0.04, 0.13), // how quickly to change the colors
       colorReach: randomRange(8, 24), // how much the hue should be aloud to shift
@@ -33,9 +49,10 @@ Polygon = React.createClass({
       movable: (Math.random() <= .75), // whether or not the polygon is movable
       movability: new Point(randomRange(0.86, 1), randomRange(0.86, 1)), // how "movable" each polygon is
       polarity: new Point(Math.random() <= .5, Math.random() <= .5) // mouse polarity (which way to go when the mouse is moved)
-    }
-  },
-  render: function () {
+    };
+  }
+
+  render() {
     // some otherwise unnecessary pointer variables to make things more legible
     var destColor = this.state.origFill;
     var colorVelocity = this.state.colorVelocity;
@@ -47,14 +64,7 @@ Polygon = React.createClass({
     // increment the darken amount. this number keeps getting bigger forever, but we take the sin of it so it fluctuates gradually between -1 and 1
     this.state.darkAmnt += colorVelocity;
 
-    // calculate the destination color
-    destColor = LightenDarkenColor(destColor, (Math.sin(this.state.darkAmnt) * colorReach));
-
-    // let React do its thing
-    return <polygon fill={destColor} transform={getTransform(movable,polarity.x,polarity.y)}
-                    points={this.state.origPoints}></polygon>;
-
-    function getTransform(movable, polarX, polarY) {
+    var getTransform = (movable, polarX, polarY) => {
       if (!movable) return ('translate(0,0)');
 
       var x = mouseVelocity.x;
@@ -67,9 +77,9 @@ Polygon = React.createClass({
       y *= movability.y;
 
       return ('translate(' + x + ' ' + y + ')');
-    }
+    };
 
-    function LightenDarkenColor(col, amt) { //https://css-tricks.com/snippets/javascript/lighten-darken-color/
+    var LightenDarkenColor = (col, amt) => { //https://css-tricks.com/snippets/javascript/lighten-darken-color/
       var usePound = false;
 
       if (col[0] == "#") {
@@ -81,34 +91,49 @@ Polygon = React.createClass({
 
       var r = (num >> 16) + amt;
 
-      if (r > 255) r = 255;
-      else if (r < 0) r = 0;
+      if (r > 255) {
+        r = 255;
+      } else if (r < 0) {
+        r = 0;
+      }
 
       var b = ((num >> 8) & 0x00FF) + amt;
 
-      if (b > 255) b = 255;
-      else if (b < 0) b = 0;
+      if (b > 255) {
+        b = 255;
+      } else if (b < 0) {
+        b = 0;
+      }
 
       var g = (num & 0x0000FF) + amt;
 
-      if (g > 255) g = 255;
-      else if (g < 0) g = 0;
-
+      if (g > 255){
+        g = 255;
+      } else if (g < 0) {
+        g = 0;
+      }
       return (usePound ? "#" : "") + (g | (b << 8) | (r << 16)).toString(16);
-    }
-  }
-});
+    };
 
-SuperCoolLion = React.createClass({
-  render: function () {
+    // calculate the destination color
+    destColor = LightenDarkenColor(destColor, (Math.sin(this.state.darkAmnt) * colorReach));
+
+    // let React do its thing
+    return <polygon fill={destColor} transform={getTransform(movable,polarity.x,polarity.y)}
+                    points={this.state.origPoints}></polygon>;
+  }
+};
+
+SuperCoolLion = class SuperCoolLion extends React.Component {
+  render() {
     return (<div><SVGArt /></div>);
   }
-});
+};
 
 
-function step(timestamp) { // onEnterFrame
+var step = () => { // onEnterFrame
   var superCoolLion = document.getElementById('super-cool-lion');
-  Meteor.startup(function () {
+  Meteor.startup(() => {
     React.render(<SuperCoolLion />, superCoolLion); // trigger a repaint
   });
   window.requestAnimationFrame(step); // keep the clock ticking
@@ -116,7 +141,7 @@ function step(timestamp) { // onEnterFrame
   // apply friction to mouse
   mouseVelocity.x *= 0.86;
   mouseVelocity.y *= 0.86;
-}
+};
 
 window.requestAnimationFrame(step);
 
